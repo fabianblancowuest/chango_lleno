311f6dd5eb12f75cf48465d96379fd8b8759f0d2
CuelloFabricio <98920036+CuelloFabricio@users.noreply.github.com>
2022-04-21 22:35:12 -0300

    Update PerfilRepository.java

--- src/main/java/com/full_monkey/repository/PerfilRepository.java
+++ src/main/java/com/full_monkey/repository/PerfilRepository.java
@@ -8,4 +8,6 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface PerfilRepository extends JpaRepository<Perfil, String> {
 
+  @Query("SELECT p FROM Perfil p WHERE p.dni = :dni")
+    public Perfil findByDni(@Param("dni") Long dni);
 }
